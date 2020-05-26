b33cf895e0a7c37085cf3837823e6052b093b55a
pradeep689 <65967180+pradeep689@users.noreply.github.com>
2020-05-26 22:23:23 +0530

    Created total terms.c
    I have print the positive integer and Fibonacci sequence into nterms on the screen

---
+++ Total Terms.c
@@ -0,0 +1,26 @@
+nterms = int(input("Total terms "))
+# first two terms
+n1, n2 = 0, 1
+count = 0
+{
+if nterms <= 0;
+ print("enter the positive integer")
+elif nterms == 1:
+ print ("Fibonacci sequence Upto",nterms,":")
+ print(n1)
+else:
+ print("Fibonacci sequence:")
+while count < nterms:
+ print(n1)
+  nth = n1 + n2
+  n1 = n2
+  n2 = nth
+  count += 1
+}
+
+
+
+
+
+
+
